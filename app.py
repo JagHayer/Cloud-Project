0b9ed3ef89c20550a968cb2c291e914f833fd221
JagHayer <48610332+JagHayer@users.noreply.github.com>
2020-11-14 22:29:52 -0500

    Update

--- app.py
+++ app.py
@@ -183,6 +183,6 @@ if __name__ == '__main__':
     reply to win £100 weekly!
     """
     #app.run(debug=True)
-    app.run(host='127.0.0.1', port=8080, debug=True)
-    #app.run(host='0.0.0.0', port=8080, debug=True)
+    #app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
